f67a93b9ab3db9ab952156f217d1d7f9f0f57915
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-12 12:20:09 -0300

    Install stable Stremio stream click guard

--- plugins/home-assistant/entry.mjs
+++ plugins/home-assistant/entry.mjs
@@ -1,7 +1,9 @@
 import { SolPluginClient, STREMIO_MCP_TOOLS } from "./lib/sol-client.mjs";
+import { installStremioStableClick } from "./lib/stremio-stable-click.mjs";
 import { installStremioLaunchGuard } from "./lib/stremio-launch-guard.mjs";
 import { installStremioDebugging } from "./lib/stremio-debug.mjs";
 
+installStremioStableClick(SolPluginClient);
 installStremioLaunchGuard(SolPluginClient);
 installStremioDebugging(SolPluginClient, STREMIO_MCP_TOOLS);
 await import("./index.mjs");
