981620d1ad5888b8eaf3f84cb3b01c5683343c1e
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-12 21:20:49 -0300

    Simplify HA-only Stremio startup path

--- plugins/home-assistant/entry.mjs
+++ plugins/home-assistant/entry.mjs
@@ -1,8 +1,6 @@
 import { SolPluginClient, STREMIO_MCP_TOOLS } from "./lib/sol-client.mjs";
 import { installHomeAssistantOnlyTvControl } from "./lib/ha-only-tv.mjs";
-import { installHaRemoteCommandNormalization } from "./lib/ha-remote-normalize.mjs";
 import { installStremioStableClick } from "./lib/stremio-stable-click.mjs";
-import { installStremioLaunchGuard } from "./lib/stremio-launch-guard.mjs";
 import { installStremioSmartPlayback } from "./lib/stremio-smart-playback.mjs";
 import { installStremioSmartCompatibility } from "./lib/stremio-smart-compat.mjs";
 import { installStremioDebugging } from "./lib/stremio-debug.mjs";
@@ -14,10 +12,8 @@ process.env.HA_SOL_TV_ENABLED = "false";
 process.env.HA_SOL_STREMIO_CENTER_TRANSPORT = "home_assistant";
 process.env.HA_SOL_STREMIO_AUTOSELECT_STREAM = "false";
 
-installHaRemoteCommandNormalization(SolPluginClient);
 installHomeAssistantOnlyTvControl(SolPluginClient);
 installStremioStableClick(SolPluginClient);
-installStremioLaunchGuard(SolPluginClient);
 installStremioSmartPlayback(SolPluginClient, STREMIO_MCP_TOOLS);
 installStremioSmartCompatibility(SolPluginClient);
 installStremioDebugging(SolPluginClient, STREMIO_MCP_TOOLS);
